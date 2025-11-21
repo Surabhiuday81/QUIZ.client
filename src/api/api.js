f80fd33e59e474854e6de5e0c89eeb120985f612
Surabhiuday81 <117470541+Surabhiuday81@users.noreply.github.com>
2025-11-21 10:06:41 +0530

    api update

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://quiz-server-m7bp.onrender.com/",
   timeout: 15000,
 });
 
